3c9f81a38b8d17bfbe52bfe806273743f7be5a6c
Avery-Jr <averys34@nycstudents.net>
2025-03-28 12:54:01 +0000

    added new files

---
+++ server.js
@@ -0,0 +1,24 @@
+// TODO: Import required modules
+// 1. Import express
+// 2. Import mongoose
+// 3. Import dotenv to load environment variables
+
+// TODO: Load environment variables
+// Hint: Use dotenv.config()
+
+// TODO: Initialize Express app
+// Hint: const app = express();
+
+// TODO: Set up middleware
+// 1. Express JSON parser
+// 2. Serve static files from the 'public' directory
+
+// TODO: Connect to MongoDB
+// Hint: Use mongoose.connect with your MONGODB_URI
+// Make sure to handle the connection promise with .then() and .catch()
+
+// TODO: Define routes
+// For now, just create a simple root route that responds with a welcome message
+
+// TODO: Start the server
+// Use the PORT from your environment variables or default to 3000
