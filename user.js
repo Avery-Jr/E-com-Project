e39489592cf8291ffa98f8600177c02753969273
Avery-Jr <averys34@nycstudents.net>
2025-04-08 13:48:08 +0000

    user.js updated

--- user.js
+++ user.js
@@ -1,15 +1,27 @@
 // TODO: Import mongoose
-
+import mongoose from "mongoose";
+const { Schema } = mongoose;
 // TODO: Create a user schema with the following fields:
-// - firstName (String, required)
-// - lastName (String, required)
-// - email (String, required, unique)
-// - password (String, required)
-// - address:
-//   - street (String, required)
-//   - apartment (String, optional)
-//   - city (String, required)
-//   - state (String, required)
-//   - zipCode (String, required)
+const userSchema = new Schema({
+  // - firstName (String, required)
+  firstName: { type: String },
+  // - lastName (String, required)
+  lastName: { type: String },
+  // - email (String, required, unique)
+  email: { type: String },
+  // - password (String, required)
+  password: { type: String },
+  // - address:
 
+  //   - street (String, required)
+  street: { type: String },
+  //   - apartment (String, optional)
+  apartment: { type: String },
+  //   - city (String, required)
+  city: { type: String },
+  //   - state (String, required)
+  state: { type: String },
+  //   - zipCode (String, required)
+  zipCode: { type: String },
+});
 // TODO: Create and export the User model
