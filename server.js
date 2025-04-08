3ebc37d75ed99a7f24263e3fb2fba694e5cce20a
Avery-Jr <averys34@nycstudents.net>
2025-04-08 13:14:07 +0000

    server.js todos done(for now)

--- server.js
+++ server.js
@@ -1,25 +1,33 @@
 // TODO: Import required modules
 // 1. Import express
-const express = require("express");
-express.static(root, [options])
+import express from "express";
 // 2. Import mongoose
-mongoose.connect("mongodb://127.0.0.1:27017/myapp");
+import mongoose from "mongoose";
 // 3. Import dotenv to load environment variables
-
+import * as dotenv from "dotenv";
 // TODO: Load environment variables
 // Hint: Use dotenv.config()
+dotenv.config();
 
 // TODO: Initialize Express app
 // Hint: const app = express();
+const app = express();
+const port = 3000;
 
 // TODO: Set up middleware
 // 1. Express JSON parser
 // 2. Serve static files from the 'public' directory
+app.use(express.json());
+console.log("db", process.env.MONGODB_URI);
 
 // TODO: Connect to MongoDB
 // Hint: Use mongoose.connect with your MONGODB_URI
 // Make sure to handle the connection promise with .then() and .catch()
-
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+  
 // TODO: Define routes
 // For now, just create a simple root route that responds with a welcome message
 app.get("/", (req, res) => {
@@ -28,3 +36,14 @@ app.get("/", (req, res) => {
 
 // TODO: Start the server
 // Use the PORT from your environment variables or default to 3000
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
+
+process.on("SIGINT", () => {
+  console.log("Stopping server...");
+  server.close(() => {
+    console.log("Server stopped. Port released.");
+    process.exit(0);
+  });
+});
