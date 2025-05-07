956c50ce62fe565584e309e125a5610fed91a4f4
Avery-Jr <averys34@nycstudents.net>
2025-05-07 13:46:26 +0000

    user creation

--- login.js
+++ login.js
@@ -1,14 +1,49 @@
 // TODO: When the DOM loads, set up event listeners for the login form
 const form = document.getElementsByClassName("form");
-form.addEventListener("click", function (e) {
-  console.log(form);
-  console.log(e.currentTarget === form);
-});
+const email = document.getElementById("email");
+const password = document.getElementById("password");
 
 // TODO: Create a function to handle the login form submission
-// 1. Prevent the default form submission
-// 2. Gather email and password data
-// 3. Send the data to your API using fetch() with POST method
-// 4. Handle the response:
-//    - If successful, store user data in localStorage and redirect to home page
-//    - If error, display appropriate error message to user
+async function logFrom(){
+  // 1. Prevent the default form submission
+  e.perventDefult();
+
+  // 2. Gather email and password data
+  const user = {
+    email: email,
+    password: password,
+  };
+  console.log(user);
+
+  // 3. Send the data to your API using fetch() with POST method
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(user, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        // Automatically converted to "username=example&password=password"
+        body: JSON.stringify({ title: input.value }),
+      });
+      logForm();
+    } catch (error) {
+      console.error(error.message);
+    }
+  });
+
+  // 4. Handle the response:
+  //    - If successful, store user data in localStorage and redirect to home page
+  //    - If error, display appropriate error message to user\
+function alert() {
+  if (error == true) {
+    alert("Error has been made, check for any invalid inputs.")
+  } else {
+    return user;
+    location.replace(
+      "https://congenial-goggles-wr5rx4j6pqvphg667-40645.app.github.dev/"
+    );
+  }
+}
+};
--- register.js
+++ register.js
@@ -1,34 +1,60 @@
 // TODO: When the DOM loads, set up event listeners for the registration form
 const form = document.getElementsByClassName("form");
-const firstName = document.getElementsById("firstName");
-const lastName = document.getElementsById("lastName");
-const email= document.getElementsById("email");
-const password = document.getElementsById("password");
-
+const firstName = document.getElementById("firstName").value;
+const lastName = document.getElementById("lastName").value;
+const email= document.getElementById("email").value;
+const password = document.getElementById("password").value;
+console.log(firstName)
 // TODO: Create a function to handle the registration form submission
-form.addEventListener("click", function (e) {
+async function regForm() {
   // 1. Prevent the default form submission
   e.perventDefult();
   // 2. Gather all form data into an object
-let name = firstName + + lastName;
-console.log(name);
   const user = {
-    username : name,
+    firstName: firstName,
+    lastName: lastName,
     email: email,
     password: password,
   };
-console.log(user)
+  console.log(user);
   // 3. Validate form data (all required fields, email format, etc.)
-const isValidEmail = () => {
-  const validity = email.value.length !== 0 && emailRegExp.test(email.value);
-  return validity;
-};
+  const isValidEmail = () => {
+    const validity = email.value.length !== 0 && emailRegExp.test(email.value);
+    return validity;
+  };
 
   // 4. Send the data to your API using fetch() with POST method
-
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(user, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        // Automatically converted to "username=example&password=password"
+        body: JSON.stringify({ title: input.value }),
+      });
+      regForm();
+    } catch (error) {
+      console.error(error.message);
+    }
+  });
   // 5. Handle the response:
   //    - If successful, store user data in localStorage and redirect to home page
   //    - If error, display appropriate error message to user
+function alert() {
+  if (error == true) {
+    alert("Error has been made, check for any invalid inputs.")
+  } else {
+    return user;
+    location.replace(
+      "https://congenial-goggles-wr5rx4j6pqvphg667-40645.app.github.dev/"
+    );
+  }
+}
 
-});
+}
+
+
 // TODO: Create helper functions for form validation
